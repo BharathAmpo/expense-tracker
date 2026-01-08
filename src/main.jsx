80c9aa4b69de8dcd57b0fdc48c8b264bba50d9dc
Bharath Kumar Ampolu <bharathkumarampolu@gmail.com>
2026-01-08 21:27:05 +0530

    clean up application entry point

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './pages/App.jsx'
-import './styles/global.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
+import "./styles/app.css"
 
-const root = createRoot(document.getElementById('root'))
-
-root.render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 )
